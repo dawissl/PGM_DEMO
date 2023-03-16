92cb162efdab6ae26a95248681c704e487743011
David Sládeček <sladecek@hradebni.cz>
2023-03-16 11:05:29 +0100

    Demo projekt

---
+++ Demo_projekt/Demo_projekt/Form1.Designer.cs
@@ -0,0 +1,240 @@
+﻿namespace Demo_projekt
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BtnBoruvka = new System.Windows.Forms.Button();
+            this.BtnCermak = new System.Windows.Forms.Button();
+            this.BtnFiala = new System.Windows.Forms.Button();
+            this.BtnJakubysyn = new System.Windows.Forms.Button();
+            this.BtnJedlicka = new System.Windows.Forms.Button();
+            this.BtnKarras = new System.Windows.Forms.Button();
+            this.BtnKaspar = new System.Windows.Forms.Button();
+            this.BtnKlamta = new System.Windows.Forms.Button();
+            this.BtnKodydek = new System.Windows.Forms.Button();
+            this.BtnMitterko = new System.Windows.Forms.Button();
+            this.BtnParnai = new System.Windows.Forms.Button();
+            this.BtnPetruska = new System.Windows.Forms.Button();
+            this.BtnSemenec = new System.Windows.Forms.Button();
+            this.BtnVojtko = new System.Windows.Forms.Button();
+            this.BtnVoracek = new System.Windows.Forms.Button();
+            this.BtnExample = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // BtnBoruvka
+            // 
+            this.BtnBoruvka.Location = new System.Drawing.Point(24, 12);
+            this.BtnBoruvka.Name = "BtnBoruvka";
+            this.BtnBoruvka.Size = new System.Drawing.Size(135, 58);
+            this.BtnBoruvka.TabIndex = 0;
+            this.BtnBoruvka.Text = "Borůvka";
+            this.BtnBoruvka.UseVisualStyleBackColor = true;
+            // 
+            // BtnCermak
+            // 
+            this.BtnCermak.Location = new System.Drawing.Point(24, 76);
+            this.BtnCermak.Name = "BtnCermak";
+            this.BtnCermak.Size = new System.Drawing.Size(135, 58);
+            this.BtnCermak.TabIndex = 1;
+            this.BtnCermak.Text = "Čermák";
+            this.BtnCermak.UseVisualStyleBackColor = true;
+            // 
+            // BtnFiala
+            // 
+            this.BtnFiala.Location = new System.Drawing.Point(24, 140);
+            this.BtnFiala.Name = "BtnFiala";
+            this.BtnFiala.Size = new System.Drawing.Size(135, 58);
+            this.BtnFiala.TabIndex = 2;
+            this.BtnFiala.Text = "Fiala";
+            this.BtnFiala.UseVisualStyleBackColor = true;
+            // 
+            // BtnJakubysyn
+            // 
+            this.BtnJakubysyn.Location = new System.Drawing.Point(24, 204);
+            this.BtnJakubysyn.Name = "BtnJakubysyn";
+            this.BtnJakubysyn.Size = new System.Drawing.Size(135, 58);
+            this.BtnJakubysyn.TabIndex = 3;
+            this.BtnJakubysyn.Text = "Jakubyšyn";
+            this.BtnJakubysyn.UseVisualStyleBackColor = true;
+            // 
+            // BtnJedlicka
+            // 
+            this.BtnJedlicka.Location = new System.Drawing.Point(24, 268);
+            this.BtnJedlicka.Name = "BtnJedlicka";
+            this.BtnJedlicka.Size = new System.Drawing.Size(135, 58);
+            this.BtnJedlicka.TabIndex = 4;
+            this.BtnJedlicka.Text = "Jedlička";
+            this.BtnJedlicka.UseVisualStyleBackColor = true;
+            // 
+            // BtnKarras
+            // 
+            this.BtnKarras.Location = new System.Drawing.Point(24, 332);
+            this.BtnKarras.Name = "BtnKarras";
+            this.BtnKarras.Size = new System.Drawing.Size(135, 58);
+            this.BtnKarras.TabIndex = 5;
+            this.BtnKarras.Text = "Karras";
+            this.BtnKarras.UseVisualStyleBackColor = true;
+            // 
+            // BtnKaspar
+            // 
+            this.BtnKaspar.Location = new System.Drawing.Point(165, 12);
+            this.BtnKaspar.Name = "BtnKaspar";
+            this.BtnKaspar.Size = new System.Drawing.Size(135, 58);
+            this.BtnKaspar.TabIndex = 6;
+            this.BtnKaspar.Text = "Kašpar";
+            this.BtnKaspar.UseVisualStyleBackColor = true;
+            // 
+            // BtnKlamta
+            // 
+            this.BtnKlamta.Location = new System.Drawing.Point(165, 76);
+            this.BtnKlamta.Name = "BtnKlamta";
+            this.BtnKlamta.Size = new System.Drawing.Size(135, 58);
+            this.BtnKlamta.TabIndex = 7;
+            this.BtnKlamta.Text = "Klamta";
+            this.BtnKlamta.UseVisualStyleBackColor = true;
+            // 
+            // BtnKodydek
+            // 
+            this.BtnKodydek.Location = new System.Drawing.Point(165, 140);
+            this.BtnKodydek.Name = "BtnKodydek";
+            this.BtnKodydek.Size = new System.Drawing.Size(135, 58);
+            this.BtnKodydek.TabIndex = 8;
+            this.BtnKodydek.Text = "Kodydek";
+            this.BtnKodydek.UseVisualStyleBackColor = true;
+            // 
+            // BtnMitterko
+            // 
+            this.BtnMitterko.Location = new System.Drawing.Point(165, 204);
+            this.BtnMitterko.Name = "BtnMitterko";
+            this.BtnMitterko.Size = new System.Drawing.Size(135, 58);
+            this.BtnMitterko.TabIndex = 9;
+            this.BtnMitterko.Text = "Mitterko";
+            this.BtnMitterko.UseVisualStyleBackColor = true;
+            // 
+            // BtnParnai
+            // 
+            this.BtnParnai.Location = new System.Drawing.Point(165, 268);
+            this.BtnParnai.Name = "BtnParnai";
+            this.BtnParnai.Size = new System.Drawing.Size(135, 58);
+            this.BtnParnai.TabIndex = 10;
+            this.BtnParnai.Text = "Parnai";
+            this.BtnParnai.UseVisualStyleBackColor = true;
+            // 
+            // BtnPetruska
+            // 
+            this.BtnPetruska.Location = new System.Drawing.Point(165, 332);
+            this.BtnPetruska.Name = "BtnPetruska";
+            this.BtnPetruska.Size = new System.Drawing.Size(135, 58);
+            this.BtnPetruska.TabIndex = 11;
+            this.BtnPetruska.Text = "Petruška";
+            this.BtnPetruska.UseVisualStyleBackColor = true;
+            // 
+            // BtnSemenec
+            // 
+            this.BtnSemenec.Location = new System.Drawing.Point(306, 12);
+            this.BtnSemenec.Name = "BtnSemenec";
+            this.BtnSemenec.Size = new System.Drawing.Size(135, 58);
+            this.BtnSemenec.TabIndex = 12;
+            this.BtnSemenec.Text = "Semenec";
+            this.BtnSemenec.UseVisualStyleBackColor = true;
+            // 
+            // BtnVojtko
+            // 
+            this.BtnVojtko.Location = new System.Drawing.Point(306, 76);
+            this.BtnVojtko.Name = "BtnVojtko";
+            this.BtnVojtko.Size = new System.Drawing.Size(135, 58);
+            this.BtnVojtko.TabIndex = 13;
+            this.BtnVojtko.Text = "Vojtko";
+            this.BtnVojtko.UseVisualStyleBackColor = true;
+            // 
+            // BtnVoracek
+            // 
+            this.BtnVoracek.Location = new System.Drawing.Point(306, 140);
+            this.BtnVoracek.Name = "BtnVoracek";
+            this.BtnVoracek.Size = new System.Drawing.Size(135, 58);
+            this.BtnVoracek.TabIndex = 14;
+            this.BtnVoracek.Text = "Voráček";
+            this.BtnVoracek.UseVisualStyleBackColor = true;
+            // 
+            // BtnExample
+            // 
+            this.BtnExample.Location = new System.Drawing.Point(550, 118);
+            this.BtnExample.Name = "BtnExample";
+            this.BtnExample.Size = new System.Drawing.Size(135, 58);
+            this.BtnExample.TabIndex = 15;
+            this.BtnExample.Text = "Ukázka";
+            this.BtnExample.UseVisualStyleBackColor = true;
+            this.BtnExample.Click += new System.EventHandler(this.BtnExample_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(823, 471);
+            this.Controls.Add(this.BtnExample);
+            this.Controls.Add(this.BtnVoracek);
+            this.Controls.Add(this.BtnVojtko);
+            this.Controls.Add(this.BtnSemenec);
+            this.Controls.Add(this.BtnPetruska);
+            this.Controls.Add(this.BtnParnai);
+            this.Controls.Add(this.BtnMitterko);
+            this.Controls.Add(this.BtnKodydek);
+            this.Controls.Add(this.BtnKlamta);
+            this.Controls.Add(this.BtnKaspar);
+            this.Controls.Add(this.BtnKarras);
+            this.Controls.Add(this.BtnJedlicka);
+            this.Controls.Add(this.BtnJakubysyn);
+            this.Controls.Add(this.BtnFiala);
+            this.Controls.Add(this.BtnCermak);
+            this.Controls.Add(this.BtnBoruvka);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Button BtnBoruvka;
+        private Button BtnCermak;
+        private Button BtnFiala;
+        private Button BtnJakubysyn;
+        private Button BtnJedlicka;
+        private Button BtnKarras;
+        private Button BtnKaspar;
+        private Button BtnKlamta;
+        private Button BtnKodydek;
+        private Button BtnMitterko;
+        private Button BtnParnai;
+        private Button BtnPetruska;
+        private Button BtnSemenec;
+        private Button BtnVojtko;
+        private Button BtnVoracek;
+        private Button BtnExample;
+    }
+}
---
+++ Demo_projekt/Demo_projekt/Form1.cs
@@ -0,0 +1,15 @@
+namespace Demo_projekt
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnExample_Click(object sender, EventArgs e)
+        {
+            new FormUkazka().ShowDialog();
+        }
+    }
+}
